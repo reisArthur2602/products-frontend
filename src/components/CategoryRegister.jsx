d5f6b382167904155f3fa5cb961fa093a0b39156
@dev.arthur <contato.arthurreis222@gmail.com>
2024-10-07 11:44:27 -0300

    feat: add form categoryRegister

--- src/components/CategoryRegister.jsx
+++ src/components/CategoryRegister.jsx
@@ -1,7 +1,16 @@
+import { Button, Card, CardContent, TextField, Typography } from "@mui/material"
 
 const CategoryRegister = () => {
   return (
-    <div>CategoryRegister</div>
+    <Card>
+      <CardContent>
+        <form style={{ display: "flex", flexDirection: "column", gap: "1rem" }}>
+          <Typography variant="h4">Cadastro de Categorias</Typography>
+          <TextField label="Nome" variant="outlined" />
+          <Button variant="contained" sx={{width:"10%"}}>Cadastrar</Button>
+        </form>
+      </CardContent>
+    </Card>
   )
 }
 
