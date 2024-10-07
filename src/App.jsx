7b324ad25599907dfaec6ccace0ccc51263844b0
@dev.arthur <contato.arthurreis222@gmail.com>
2024-10-07 11:15:08 -0300

    feat: add menu Drawer

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,12 @@
-import { useState } from "react";
-import ProductRegister from "./components/ProductRegister";
-import ProductList from "./components/ProductList";
-import CategoryRegister from "./components/CategoryRegister";
-import CategoryList from "./components/CategoryList";
+
+import MenuDrawer from "./components/MenuDrawer";
 
 const App = () => {
-  const [isVisible, setIsVisible] = useState(1);
 
-  const selectComponent = () => {
-    switch (isVisible) {
-      case 1:
-        return <ProductRegister />;
-      case 2:
-        return <ProductList />;
-      case 3:
-        return <CategoryRegister />;
-      case 4:
-        return <CategoryList />;
-    }
-  };
   return (
     <div>
-      <button onClick={()=> setIsVisible(1)}>Cadastro de Produtos</button>
-      <button onClick={()=> setIsVisible(2)}>Listagem de produtos</button>
-      <button onClick={()=> setIsVisible(3)}>Cadastro de Categorias</button>
-      <button onClick={()=> setIsVisible(4)}>Listagem de Categorias</button>
-     {selectComponent()}
+      <MenuDrawer/>
+     
     </div>
   );
 };
---
+++ src/components/MenuDrawer.jsx
@@ -0,0 +1,199 @@
+import * as React from "react";
+import { styled, useTheme } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import MuiAppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import CssBaseline from "@mui/material/CssBaseline";
+import List from "@mui/material/List";
+import Typography from "@mui/material/Typography";
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import ProductRegister from "./ProductRegister";
+import ProductList from "./ProductList";
+import CategoryRegister from "./CategoryRegister";
+import CategoryList from "./CategoryList";
+import { Container } from "@mui/material";
+
+const drawerWidth = 240;
+
+const Main = styled("main", { shouldForwardProp: (prop) => prop !== "open" })(
+  ({ theme }) => ({
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create("margin", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginRight: -drawerWidth,
+
+    position: "relative",
+    variants: [
+      {
+        props: ({ open }) => open,
+        style: {
+          transition: theme.transitions.create("margin", {
+            easing: theme.transitions.easing.easeOut,
+            duration: theme.transitions.duration.enteringScreen,
+          }),
+          marginRight: 0,
+        },
+      },
+    ],
+  })
+);
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme }) => ({
+  transition: theme.transitions.create(["margin", "width"], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  variants: [
+    {
+      props: ({ open }) => open,
+      style: {
+        width: `calc(100% - ${drawerWidth}px)`,
+        transition: theme.transitions.create(["margin", "width"], {
+          easing: theme.transitions.easing.easeOut,
+          duration: theme.transitions.duration.enteringScreen,
+        }),
+        marginRight: drawerWidth,
+      },
+    },
+  ],
+}));
+
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+  justifyContent: "flex-start",
+}));
+
+export default function MenuDrawer() {
+  const theme = useTheme();
+  const [isVisible, setIsVisible] = React.useState(1);
+  const [open, setOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  const selectComponent = () => {
+    switch (isVisible) {
+      case 1:
+        return <ProductRegister />;
+      case 2:
+        return <ProductList />;
+      case 3:
+        return <CategoryRegister />;
+      case 4:
+        return <CategoryList />;
+    }
+  };
+  return (
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <AppBar position="fixed" open={open}>
+        <Toolbar>
+          <Typography variant="h6" noWrap sx={{ flexGrow: 1 }} component="div">
+            Gerenciador de Produtos
+          </Typography>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="end"
+            onClick={handleDrawerOpen}
+            sx={[open && { display: "none" }]}
+          >
+            <MenuIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+      <Main open={open}>
+        <DrawerHeader />
+
+        <Container>
+          {selectComponent()}
+        </Container>
+
+      </Main>
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          "& .MuiDrawer-paper": {
+            width: drawerWidth,
+          },
+        }}
+        variant="persistent"
+        anchor="right"
+        open={open}
+      >
+        <DrawerHeader>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === "rtl" ? (
+              <ChevronLeftIcon />
+            ) : (
+              <ChevronRightIcon />
+            )}
+          </IconButton>
+        </DrawerHeader>
+        <Divider />
+        <List>
+          <ListItem key={1} disablePadding onClick={() => setIsVisible(1)}>
+            <ListItemButton>
+              <ListItemIcon>
+                <InboxIcon />
+              </ListItemIcon>
+              <ListItemText primary={"Cadastro de Produtos"} />
+            </ListItemButton>
+          </ListItem>
+
+          <ListItem key={2} disablePadding onClick={() => setIsVisible(2)}>
+            <ListItemButton>
+              <ListItemIcon>
+                <InboxIcon />
+              </ListItemIcon>
+              <ListItemText primary={"Listagem de Produtos"} />
+            </ListItemButton>
+          </ListItem>
+
+          <ListItem key={3} disablePadding onClick={() => setIsVisible(3)}>
+            <ListItemButton>
+              <ListItemIcon>
+                <InboxIcon />
+              </ListItemIcon>
+              <ListItemText primary={"Cadastro de Categorias"} />
+            </ListItemButton>
+          </ListItem>
+
+          <ListItem key={4} disablePadding onClick={() => setIsVisible(4)}>
+            <ListItemButton>
+              <ListItemIcon>
+                <InboxIcon />
+              </ListItemIcon>
+              <ListItemText primary={"Listagem de Categorias"} />
+            </ListItemButton>
+          </ListItem>
+        </List>
+      </Drawer>
+    </Box>
+  );
+}
