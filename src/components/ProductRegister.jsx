70e5358f1b811e77b49713ebc8902994e01be1d0
@dev.arthur <contato.arthurreis222@gmail.com>
2024-10-07 11:44:39 -0300

    feat: add form productRegister

--- src/components/ProductRegister.jsx
+++ src/components/ProductRegister.jsx
@@ -1,5 +1,25 @@
+import {
+  Button,
+  Card,
+  CardContent,
+  TextField,
+  Typography,
+} from "@mui/material";
+
 const ProductRegister = () => {
-  return <div>ProductRegister</div>;
+  return (
+    <Card>
+      <CardContent>
+        <form style={{ display: "flex", flexDirection: "column", gap: "1rem" }}>
+          <Typography variant="h4">Cadastro de Produtos</Typography>
+          <TextField label="Nome" variant="outlined" />
+          <TextField label="Preço" variant="outlined" />
+          <TextField label="Categoria" variant="outlined" />
+          <Button variant="contained" sx={{width:"10%"}}>Cadastrar</Button>
+        </form>
+      </CardContent>
+    </Card>
+  );
 };
 
 export default ProductRegister;
