81c62509c876b691c638c58386ab45f188587670
@dev.arthur <contato.arthurreis222@gmail.com>
2024-10-07 11:29:58 -0300

    feat: add table Categorylist

--- src/components/CategoryList.jsx
+++ src/components/CategoryList.jsx
@@ -1,7 +1,50 @@
-const CategoryList = () => {
-  return (
-    <div>CategoryList</div>
-  )
-}
+import {
+  Box,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
+} from "@mui/material";
 
-export default CategoryList
+const CategoryList = () => {
+  const rows = [
+    { id: 1, name: "Informática" },
+    { id: 2, name: "Papelaria" },
+    { id: 2, name: "Casa" },
+    
+  ];
+
+  return (
+    <Box component="div" display="flex" flexDirection="column" gap="1rem">
+      <Typography variant="h4">Listagem de Categorias</Typography>
+
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Nome</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map((row) => (
+              <TableRow
+                key={row.id}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {row.name}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Box>
+  );
+};
+
+export default CategoryList;
